94b26af0096540ff08533f427bb0a02494524623
shohankazi <shohankazi012@gmail.com>
2023-02-05 11:17:56 +0600

    code added

--- Hello.c
+++ Hello.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
-int main(){
-    string get_string = ()
-    printf("Hello World!\n Hello I am Shohan");
+// sum and Average of three numbers
+#include <stdio.h>
+int main()
+{
+    int a, b, c, sum;
+    float avg;
+    printf("Enter three integer value: ");
+    scanf("%d %d %d", &a, &b, &c);
+    sum = a + b + c;
+    avg = sum / 3;
+    printf("\n The summation of three number is= %d", sum);
+    printf("\n Average of three number is =%f", avg);
 };
-    
