9f2f2dd20e5b3558b85272385e35d96d2001a699
shohankazi <shohankazi012@gmail.com>
2023-03-24 10:40:30 +0600

    code added

--- Hello.c
+++ Hello.c
@@ -1,13 +1,5 @@
-// sum and Average of three numbers
-#include <stdio.h>
-int main()
-{
-    int a, b, c, sum;
-    float avg;
-    printf("Enter three integer value: ");
-    scanf("%d %d %d", &a, &b, &c);
-    sum = a + b + c;
-    avg = sum / 3;
-    printf("\n The summation of three number is= %d", sum);
-    printf("\n Average of three number is =%f", avg);
-};
+// Hello World Program
+#include<stdio.h>
+int main(){
+    printf("Munna is the shortest in the world!\n");
+}
