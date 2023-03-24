6deb3ba76f3866a1816e020fd58f28ff9ea3f6d2
shohankazi <shohankazi012@gmail.com>
2023-03-24 11:18:36 +0600

    code added

--- variable.c
+++ variable.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main()
 {
-    char characterName[] = "Munna";
+    char characterName[] = "Didar";
     int characterAge = 35;
     printf("There once was a man named %s \n", characterName);
     printf("He was %d years old.\n", characterAge);
