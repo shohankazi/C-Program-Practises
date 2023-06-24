606b28808d1569c9b43a75968d0c659a4b93c2ff
shohankazi <shohankazi012@gmail.com>
2023-06-24 20:18:42 +0600

    code added

--- new.c
+++ new.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 #include<conio.h>
 int main(){
-    int a ,b,c,sum;
+    int a,b,c,sum,subtract, multiply,division,square;
     a = 5;
     b = 6;
-    c = 7;
+    c = 10;
+    square = a*a;
     sum = a + b + c;
-    printf("%d\n",sum);
+    subtract = c-b-a;
+    multiply = a * b;
+    division = c / a;
+    printf("%d\n %d\n %d\n %d\n %d\n",sum,subtract,multiply,division,square);
     printf("I am Hero Arpon! \n");
     printf("I am a good boy! \n");
 }
