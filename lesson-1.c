591e648dbaebb993e08b9843661dcb82843ef151
shohankazi <shohankazi012@gmail.com>
2023-07-08 20:06:32 +0600

    c program explained

---
+++ lesson-1.c
@@ -0,0 +1,26 @@
+// #include<stdio.h>
+// int main(){
+//     char a,b;
+//     printf("Enter your First name ");
+//     scanf("%c",&a);
+//     printf("Enter your Last name ");
+//     scanf("%c",&b);
+// }
+
+// #include <stdio.h>
+// int main(){
+//     printf("Hello world!\n");
+//     printf("I am Arpon\n");
+//     printf("Hello Rakib!\n");
+// }
+
+
+#include<stdio.h>
+int main(){
+    int a, b, sum;
+    a = 10;
+    b = 20;
+    sum = a + b;
+    printf("%d\n",sum);
+    printf("This is your sum = %d",sum);
+}
