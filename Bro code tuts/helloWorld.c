e125e14ece0e205884d559a2b53cd7afb2e230d2
shohankazi <shohankazi012@gmail.com>
2023-08-23 12:48:43 +0600

    C programming practising again

---
+++ Bro code tuts/helloWorld.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int x;
+    x = 123;
+    int y = 321;
+
+    int age = 21;
+    float gpa = 2.05;
+    char grade = 'C';
+    char name[] = "Bro";
+    printf("Hello %s\n",name);
+    printf("You are %d years old\n",age);
+    printf("Your average grade is %c\n",grade);
+    printf("Your gpa is %f\n",gpa);
+    return 0;
+
+}
---
+++ random works/hypotenuse.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int hyp, perpendicular, base,result;
+    perpendicular = 4;
+    base = 5;
+    hyp = perpendicular*perpendicular+base*base;
+    result = int sqrt(hyp);
+    printf("%f",hyp);
+    printf("hello world");
+}
