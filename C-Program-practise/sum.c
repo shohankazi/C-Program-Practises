34851250c27c6c14529a3d5a0ad8325c2740a8b9
shohankazi <shohankazi012@gmail.com>
2023-11-11 11:02:16 +0600

    Even, odd program completed

---
+++ C-Program-practise/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a;
+    a = 6;
+    if(a % 2 == 0){
+        printf("this is a even number %d", a);
+    }
+    else{
+        printf("this is a odd %d number", a);
+    }
+    return 0;
+}
