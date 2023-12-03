24b5ab21d1dedd4fd3391720d6df55c5fad2cc40
shohankazi <shohankazi012@gmail.com>
2023-12-03 23:58:44 +0600

    Greatest common divisor program added

---
+++ C-Program-practise/GCD.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main(){
+    int A,B,temp;
+    scanf('%d %d',&A,&B);
+    if(A > B){
+        temp = A;
+        A = B;
+        B = temp;
+    }
+    while((B%A)!= 0){
+        temp = B % A;
+        B = A;
+        A = temp;
+    }
+    printf("The GCD is %d",A);
+}
