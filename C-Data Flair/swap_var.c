3b0d8c10e094040dbfce6c937f531a927f50e104
shohankazi <shohankazi012@gmail.com>
2023-12-10 18:41:58 +0600

    Swap program without using third variable

--- C-Data Flair/swap_var.c
+++ C-Data Flair/swap_var.c
@@ -12,3 +12,18 @@ void main(){
     b = c;
     printf("After swapping %d %d",a,b);
 }
+
+// Swapping without using third variable
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int a, b,c;
+    printf("Enter two number");
+    scanf("%d %d", &a, &b);
+    printf("Before swapping %d %d",a,b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swapping %d %d",a,b);
+}
