c02b008e1fb0bd8d1847d22fabba11a13a9adbd5
shohankazi <shohankazi012@gmail.com>
2023-12-11 20:06:25 +0600

    Check a character is vowel or consonant

--- C-Data Flair/condition.c
+++ C-Data Flair/condition.c
@@ -1,5 +1,20 @@
+// Program to check whether the number is positive or negative
+
+/* 
+
 #include<stdio.h>
 int main(){
     int n = -23;
     n > 0 ? printf("%d is positive",n) : printf("%d is negative",n);
+}
+
+*/
+
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c",&ch);
+    (ch == 'a'||ch == 'e'||ch == 'i'||ch == 'o'||ch == 'u'||ch == 'A'||ch == 'E'||ch == 'I'||ch == 'O'||ch == 'U') ? printf(" %c is vowel",ch):printf("%c is consonant",ch);
+    return 0;
 }
