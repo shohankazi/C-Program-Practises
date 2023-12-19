80d1c7a116a9df66fa2418a092b110e06d0e4aeb
shohankazi <shohankazi012@gmail.com>
2023-12-19 18:52:41 +0600

    String copy vs String concatenation program added

---
+++ C-Data Flair/strcpy_vs_strcat.c
@@ -0,0 +1,32 @@
+// =====>> String Copy <<======
+
+/*
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main(){
+    char s1[100], s2[100];
+    printf("Enter a string: ");
+    gets(s1);
+    puts(s1);
+    strcpy(s2,s1);
+    printf("Copied into second string: %s",s2);
+}
+*/
+
+// =====>> String concatenation <<======
+
+/*
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main(){
+    char s1[100], s2[100];
+    printf("Enter first string: ");
+    gets(s1);
+    printf("Enter second string: ");
+    gets(s2);
+    strcat(s1, s2);
+    puts(s1);
+}
+*/
