b1023f5c88028b589e814ae1027ebfb7157bb9be
shohankazi <shohankazi012@gmail.com>
2023-12-24 21:27:19 +0600

    Lower matrix added

---
+++ C-Data Flair/lower_triangle.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int arr[30][30], m, n,r,c;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d",&m,&n);
+    printf("Enter the values of the matrices: ");
+    for (r = 0; r < m; r++)
+    {
+        for(c = 0; c < n; c++){
+            scanf("%d", &arr[r][c]);
+        }
+    }
+    for (r = 0; r < m; r++)
+    {
+        for(c = 0; c < n; c++){
+            printf("%5d", arr[r][c]); // Print the current values of matrix
+        }
+        printf("\n");
+    }
+    if (m != n)
+    {
+        printf("It is not a diagonal matrix");
+    }
+    else{
+        printf("Lower Triangle: \n");
+        for (r = 0; r < m; r++)
+        {
+            for (c = 0; c < n; c++)
+            {
+                if(r >= c){
+                    printf("%5d",arr[r][c]); // Print the lower triangle of the matrix
+                }
+            }
+            printf("\n");
+            
+        }
+        
+    }
+    return 0;
+}
