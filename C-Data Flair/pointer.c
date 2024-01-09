12c0dc60292167c550be7598c75a77276e7bcbae
shohankazi <shohankazi012@gmail.com>
2024-01-09 11:25:09 +0600

    Pointer program added

---
+++ C-Data Flair/pointer.c
@@ -0,0 +1,47 @@
+// Pointer Practical
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=10,*p,*r,b=50;
+    p=&a;
+    printf(" a = %d", *p);
+    r=&b;
+    printf("\n b = %d\n", *r);
+    ++*p;  //a=11
+    printf("increment of a = %d\n", *p);
+    ++*r;    //b=51
+    printf("increment of b = %d\n", *r);
+    a=a+5; //a=16
+    printf("%d\n", a);
+    b=b+5;//b=56
+    printf("%d\n", b);
+    r=&a;
+    printf("%d\n", a);
+    ++*p; //a=17
+    printf("%d\n", *p);
+    ++*r;  // a=18
+    printf("%d\n", *r);
+
+    // printf("a= %d b= %d *p= %d *r= %d ",a,b,*p,*r);
+
+
+    //  p=&a;
+    //  ++*p;  //Value 
+    //  printf("\n\n %d \t %d ",a,*p);
+    //  ++p;  //Address 
+    //  printf(" \n\n %d \t  %d ",a,*p);
+
+
+    // int a=10,*p,**r;
+    // p=&a;
+    // r=&p;
+    // printf("\nValue of a %d",a);
+    // printf("\nValue of p %u",p);
+    // printf("\nValue of *p %d",*p);
+    // printf("\nValue of r %u",r);
+    // printf("\nValue of *r %u",*r);
+    // printf("\nValue of **r %d",**r);
+    // ++**r;
+    // printf("\nValue of a %d",a);
+}
