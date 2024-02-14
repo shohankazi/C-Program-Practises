35449ad704a90e66367809730405c5baa1d5a508
shohankazi <shohankazi012@gmail.com>
2024-02-14 13:51:57 +0600

    Coding Journey started again

--- C-Data Flair/pointer.c
+++ C-Data Flair/pointer.c
@@ -3,25 +3,29 @@
 #include<conio.h>
 void main()
 {
-    int a=10,*p,*r,b=50;
-    p=&a;
-    printf(" a = %d", *p);
-    r=&b;
-    printf("\n b = %d\n", *r);
-    ++*p;  //a=11
-    printf("increment of a = %d\n", *p);
-    ++*r;    //b=51
-    printf("increment of b = %d\n", *r);
-    a=a+5; //a=16
-    printf("%d\n", a);
-    b=b+5;//b=56
-    printf("%d\n", b);
-    r=&a;
-    printf("%d\n", a);
-    ++*p; //a=17
-    printf("%d\n", *p);
-    ++*r;  // a=18
-    printf("%d\n", *r);
+    int a;
+    a = 50;
+    printf("Value is %d\n", a);
+    printf("Address is %d", &a);
+    // int a=10,*p,*r,b=50;
+    // p=&a;
+    // printf(" a = %d", *p);
+    // r=&b;
+    // printf("\n b = %d\n", *r);
+    // ++*p;  //a=11
+    // printf("increment of a = %d\n", *p);
+    // ++*r;    //b=51
+    // printf("increment of b = %d\n", *r);
+    // a=a+5; //a=16
+    // printf("%d\n", a);
+    // b=b+5;//b=56
+    // printf("%d\n", b);
+    // r=&a;
+    // printf("%d\n", a);
+    // ++*p; //a=17
+    // printf("%d\n", *p);
+    // ++*r;  // a=18
+    // printf("%d\n", *r);
 
     // printf("a= %d b= %d *p= %d *r= %d ",a,b,*p,*r);
 
