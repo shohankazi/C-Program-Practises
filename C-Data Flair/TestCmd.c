012b82d8fd84109d13c231c7359b47ec4169ff00
shohankazi <shohankazi012@gmail.com>
2024-03-02 10:54:43 +0600

    command line argument code added

---
+++ C-Data Flair/TestCmd.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+void main(int argc, char *argv[]){
+    printf("\nTotal argument is %d",argc);
+    printf("\nFirst argument is %s",argv[0]);
+    printf("\nSecond argument is %s",argv[1]);
+    printf("\nThird argument is %s",argv[2]);
+}
