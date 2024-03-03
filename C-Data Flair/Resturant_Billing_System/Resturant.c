71f2baf99a66b5849c89bada7613cca666459c06
shohankazi <shohankazi012@gmail.com>
2024-03-03 19:25:38 +0600

    Restaurant Billing Program Completed

--- C-Data Flair/Resturant_Billing_System/Resturant.c
+++ C-Data Flair/Resturant_Billing_System/Resturant.c
@@ -12,7 +12,7 @@ struct Product
 struct Product p[3];
 void main(){
     int i,total = 0;
-    for ( i = 0; i < 3; i++)
+    for ( i = 0; i < 2; i++)
     {   fflush(0);
         printf("\n Enter Product id: ");
         scanf("%d", &p[i].pid);
@@ -27,13 +27,14 @@ void main(){
         scanf("%d", &p[i].qty);
     }
 printf("\n --------------- Product Details------------");
-printf("\nProduct ID\tProduct Name\tPrice\tQuantity\tAmount\n");
-for ( i = 0; i < 3; i++)
+printf("\nID\tName\tPrice\tQuantity\tAmount\n");
+for ( i = 0; i < 2; i++)
 {
-    printf("\t%d\t%s\t%d\t%d", p[i].pid, p[i].pname, p[i].rate, p[i].qty,(p[i].rate * p[i].qty));
+    printf("%d\t%s\t%d\t%d\t\t%d", p[i].pid, p[i].pname, p[i].rate, p[i].qty,(p[i].rate * p[i].qty));
     total = total + p[i].rate * p[i].qty;
     printf("\n");
     }
-    printf("\n------------------------");
-    printf("\n\t\t\tTotal Amount: %d",total);
+    printf("\n--------------------------------------------");
+    printf("\n\t\tTotal Amount: %d",total);
+    printf("\n--------------------------------------------");
 }
